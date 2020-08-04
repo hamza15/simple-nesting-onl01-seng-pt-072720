678d3a99fcdcb999a2a8b87e5a4c8b22891e8a6f
Sardar Hamza Naqi <sardarhamzanaqi@gmail.com>
2020-08-04 03:38:20 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -16,6 +16,7 @@ def hopper
         }
      }
 
+programmer_hash[:grace_hopper]
 
 end
 
@@ -37,6 +38,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,14 +57,13 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     
+programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
 # add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#    :languages => ["LISP", "C"]
-# }
+
 
 	programmer_hash = 
  		{
@@ -79,7 +80,9 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
+     
+programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby",
+programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP", "C"]
     
 end
 
